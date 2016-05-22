81a53200d5d1031482de1c91fc504fc6b295d008
petermichuncc <plarrimo@uncc.edu>
2016-05-22 17:36:02 -0400

    minor update.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
     
         // also include ngRoute for all our routing needs
-    var myApp = angular.module('myApp', ['ngRoute']);
+    var myApp = angular.module('myApp', ['ngRoute','ngMaterial']);
 
     // configure our routes
     myApp.config(function($routeProvider) {
