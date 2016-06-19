ca048631c6cdc73bd59e8fe3c0f9a6b7a1db2849
petermichuncc <plarrimo@uncc.edu>
2016-06-18 22:35:04 -0400

    started working on the home page

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
     
         // also include ngRoute for all our routing needs
+/*
     var myApp = angular.module('myApp', ['ngRoute']);
 
-    // configure our routes
+   
     myApp.config(function($routeProvider) {
         $routeProvider
 
-            // route for the home page
+           
             .when('/', {
                 templateUrl : 'pages/start.html',
                 controller  : 'startController'
@@ -17,13 +18,13 @@
                 templateUrl : 'pages/home.html',
                 controller  : 'startController'
             })
-            // route for the about page
+
             .when('/pricing', {
                 templateUrl : 'pages/pricing.html',
                 controller  : 'pricingController'
             })
 
-            // route for the contact page
+          
             .when('/contact', {
                 templateUrl : 'pages/contact.html',
                 controller  : 'contactController'
@@ -34,14 +35,12 @@
             });
     });
 
-    // create the controller and inject Angular's $scope
+ 
     myApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        //$scope.message = '';
+       
     });
      myApp.controller('startController', function($scope) {
-        // create a message to display in our view
-        //$scope.message = '';
+       
         $scope.display = function() {
   
         $scope.alertDisplayed = true;
@@ -54,7 +53,7 @@
     });
 
     myApp.controller('pricingController', function($scope) {
-       // $scope.message = '';
+   
     });
 
    myApp.controller('contactController', function($scope) {
@@ -67,3 +66,4 @@
    myApp.controller('AppCtrl', function ($scope) {
     $scope.hidden = true;
 });
+*/
